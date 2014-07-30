0f22be77414db8955886156c135806bd345e893d
Dan Guest <dguest@cern.ch>
2014-07-30 19:06:36 -0400

    minor

--- dump_colors.py
+++ dump_colors.py
@@ -2,7 +2,7 @@
 """dump sbottom colors"""
 import ROOT
 from ROOT import TColor, TString
-
+import warnings
 
 color_dict = {
     'other':('#FFA500','orange'),
@@ -18,14 +18,22 @@ signal_list = [
 for num, sig in enumerate(signal_list):
     color_dict['signal_{}'.format(num)] = sig
 
+prt_fmt = '| {:10} | {:11} | {:5.3f} | {:5.3f} | {:5.3f} | {rn:11} |'
+leg_fmt = prt_fmt.replace(':5.3f',':5').replace(':',':^')
+legs = ['*process*', '*html name*'] + ['*{}*'.format(x) for x in 'rgb']
+print leg_fmt.format(*legs, rn='*root name*')
 for proc, (color, cname) in color_dict.items():
     # data = TString(color).Data()
     rgb_str = [color[x*2+1:x*2+3] for x in range(3)]
     rgb = [float(int(x, 16)) / 255 for x in rgb_str]
     enum = TColor.GetColor(color)
     back = ROOT.gROOT.GetColor(enum)
-    prt_fmt = '| {:10} | {:10} | {:.3f} {:.3f} {:.3f} | {:10} |'
+    root_rgb = (back.GetRed(), back.GetGreen(), back.GetBlue())
+    delta = [root - original for root, original in zip(root_rgb, rgb)]
+    if any(x > 0.001 for x in delta):
+        wrn = "root and html {} not the same, delta = {}".format(cname, delta)
+        warnings.warn(wrn)
     out_str = prt_fmt.format(
-        proc, cname,
-        back.GetRed(), back.GetGreen(), back.GetBlue(), back.GetName())
+        proc, cname, *rgb
+        , rn=back.GetName())
     print out_str
