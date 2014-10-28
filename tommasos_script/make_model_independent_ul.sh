9ab5c8d67bdb31a584c33541c43e12b074bc0622
Dan Guest <dguest@cern.ch>
2014-10-28 16:10:41 -0400

    minor changes

--- tommasos_script/make_model_independent_ul.sh
+++ tommasos_script/make_model_independent_ul.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# safety set: quit on nonzero return code, quit on unset variable
+set -eu
+
 export PYTHONPATH=$(dirname $(which HistFitter.py))/../python
 
 # set counts as environment variables. This is a horrible hack, but
@@ -12,6 +15,10 @@ ndata=([150]=19   [200]=11   [250]=4   )
 nbkgd=([150]=29.6 [200]=16.3 [250]=8.21)
 bgerr=([150]=5.57 [200]=3.46 [250]=1.94)
 
+# we make a subdirectory to fill with all HistFitter's barf
+mkdir -p barf
+cd barf
+
 # build workspaces
 for reg in 150 200 250 ; do
     echo ===================== doing $reg ===========================
@@ -19,5 +26,16 @@ for reg in 150 200 250 ; do
     export NDATA=${ndata[$reg]}
     export NBKGD=${nbkgd[$reg]}
     export BGERR=${bgerr[$reg]}
-    HistFitter.py -w -f UpperLimit2013.py
+    HistFitter.py -w -f ../UpperLimit2013.py > /dev/null
 done
+
+# NOTE: I've hacked UpperLimitTable.py to simplify the arguments, this
+# won't work unless you check out the version of Histfitter here:
+# https://github.com/dguest/HistFitter
+# Fortunately, the script will quit at this point if anything goes wrong
+# so you can run UpperLimitTable yourself.
+
+WS_TAIL=SPlusB_combined_NormalMeasurement_model.root
+WORKSPACES=$(echo results/UpperLimitScharm{150,200,250}/$WS_TAIL)
+UpperLimitTable.py $WORKSPACES > /dev/null
+UpperLimitTable.py $WORKSPACES -n 30
